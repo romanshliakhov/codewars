18d8feb42484809554d6b547e08ae7f95c970a23
Roman <romanshliakhov@gmail.com>
2022-03-30 18:23:50 +0300

    added ex 3

--- js/main.js
+++ js/main.js
@@ -26,6 +26,14 @@
 //   return sum;
 // } 
 
+// Ex 3. Convert a Number to a String!
+// We need a function that can transform a number into a string.
+// What ways of achieving this do you know?
+
+// function numberToString(num) {
+//     // Return a string of the number here!
+//     return num.toString();
+// }
 
 
 
